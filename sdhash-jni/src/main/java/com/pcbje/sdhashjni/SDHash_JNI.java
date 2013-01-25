b5295da8b8bdc76d0e41b7492f64e8b4cbf03fab
Petter Christian Bjelland <petter.bjelland@gmail.com>
2013-01-25 19:55:01 +0100

    Import cleanup

--- sdhash-jni/src/main/java/com/pcbje/sdhashjni/SDHash_JNI.java
+++ sdhash-jni/src/main/java/com/pcbje/sdhashjni/SDHash_JNI.java
@@ -6,8 +6,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 

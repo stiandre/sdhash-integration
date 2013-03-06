95311c2612e4c2facfccd2f1414800ed55cc88f4
Petter Chr. Bjelland <petter.bjelland@gmail.com>
2013-03-06 16:22:42 +0100

    load boost

--- sdhash-jni/src/main/java/com/pcbje/sdhashjni/SDHash_JNI.java
+++ sdhash-jni/src/main/java/com/pcbje/sdhashjni/SDHash_JNI.java
@@ -20,31 +20,15 @@ public class SDHash_JNI {
 
     static {
         try {
-            if (os.indexOf("linux") < 0) {        
+            if (os.indexOf("linux") < 0) {
                 throw new Error("OS " + os + " is not supported");
             }
-            
+
             String libsdhash = "libsdhash_jni.so";
-            
-            Logger.getLogger(SDHash_JNI.class.getName()).log(Level.INFO, "Loading {0}...", libsdhash);
+            String libboost = "libboost_system.so.1.49.0";
 
-            InputStream in = SDHash_JNI.class.getClassLoader().getResourceAsStream(libsdhash);
-
-            File temp = File.createTempFile(libsdhash, "");
-
-            OutputStream out = new FileOutputStream(temp);
-
-            byte[] buffer = new byte[1024];
-
-            int len;
-
-            while ((len = in.read(buffer)) != -1) {
-                out.write(buffer, 0, len);
-            }
-
-            out.close();
-
-            System.load(temp.getAbsolutePath());
+            loadLib(libsdhash);
+            loadLib(libboost);
 
         } catch (IOException ex) {
             Logger.getLogger(SDHash_JNI.class.getName()).log(Level.SEVERE, null, ex);
@@ -52,19 +36,41 @@ public class SDHash_JNI {
         }
     }
 
+    private static void loadLib(String libsdhash) throws IOException {
+        Logger.getLogger(SDHash_JNI.class.getName()).log(Level.INFO, "Loading {0}...", libsdhash);
+
+        InputStream in = SDHash_JNI.class.getClassLoader().getResourceAsStream(libsdhash);
+
+        File temp = File.createTempFile(libsdhash, "");
+
+        OutputStream out = new FileOutputStream(temp);
+
+        byte[] buffer = new byte[1024];
+
+        int len;
+
+        while ((len = in.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
+        }
+
+        out.close();
+
+        System.load(temp.getAbsolutePath());
+    }
+
     public static void main(String[] args) throws Exception {
         StringBuilder digests = new StringBuilder();
 
-        for (int i=0; i<args.length; i++) {
+        for (int i = 0; i < args.length; i++) {
             digests.append(digest(jni, args[i]));
         }
-        
+
         File tmpfile = File.createTempFile("sdhash-compare", "txt");
 
-	FileOutputStream fos = new FileOutputStream(tmpfile);
+        FileOutputStream fos = new FileOutputStream(tmpfile);
 
         fos.write(digests.toString().getBytes());
-        
+
         fos.close();
 
         System.out.println(jni.compare(digests.toString(), 16));
@@ -73,7 +79,7 @@ public class SDHash_JNI {
     public static String compare(String sdbf) {
         return jni.compare(sdbf, 16);
     }
-    
+
     public static String digestBytes(String filename, byte[] content) {
         return jni.getSDBF(filename, content, content.length);
     }

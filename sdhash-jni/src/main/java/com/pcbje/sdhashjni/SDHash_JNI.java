b47e00ad4e6c49bd61a09e1a79847415abb11fe4
pcbje <pcbje@pcbje.(none)>
2013-03-26 12:47:26 +0100

    ship with prebuilt boost

--- sdhash-jni/src/main/java/com/pcbje/sdhashjni/SDHash_JNI.java
+++ sdhash-jni/src/main/java/com/pcbje/sdhashjni/SDHash_JNI.java
@@ -12,6 +12,12 @@ import java.util.logging.Logger;
 
 public class SDHash_JNI {
 
+    private static final String[] boostlib = new String[]{
+        "libboost_thread.so.1.49.0",
+        "libboost_system.so.1.49.0",        
+        "libboost_program_options.so.1.49.0",
+        "libboost_filesystem.so.1.49.0",        
+    };
     private static final String os = System.getProperty("os.name").toLowerCase();
     private static final SDHash_JNI jni = new SDHash_JNI();
 
@@ -20,36 +26,49 @@ public class SDHash_JNI {
     private native String compare(ByteBuffer sdbfs, int threshold);
 
     static {
-        try {
+        try {            
             String libsdhash;
-            
+            String boostdir;
+
             if (os.contains("linux")) {
                 libsdhash = "libsdhash_jni-linux-x64.so";
-            }
-            else {
+                boostdir = "boost/linux-x64";
+            } else {
                 libsdhash = "libsdhash_jni.so";
+                boostdir = "boost/osx";
             }
-
-            loadLib(libsdhash);
-
-            Logger.getLogger(SDHash_JNI.class.getName()).log(Level.INFO, "Loaded {0}", libsdhash);
+            
+            for (String lib : boostlib) {
+                loadLib(boostdir, lib);
+            }
+            
+            loadLib(null, libsdhash);
         } catch (IOException ex) {
             Logger.getLogger(SDHash_JNI.class.getName()).log(Level.SEVERE, null, ex);
             System.exit(1);
         }
     }
 
-    private static void loadLib(String libsdhash) throws IOException {
-        InputStream in = SDHash_JNI.class.getClassLoader().getResourceAsStream(libsdhash);
-
-        File temp = File.createTempFile(libsdhash, "");
-
+    private static void loadLib(String root, String libname) throws IOException {
+        String path = "";
+        
+        if (root != null) {
+            path += root + "/";
+        }
+        
+        path += libname;
+                        
+        Logger.getLogger(SDHash_JNI.class.getName()).log(Level.INFO, "Loading {0}", path);
+        
+        InputStream in = SDHash_JNI.class.getClassLoader().getResourceAsStream(path);
+        
+        File temp = File.createTempFile(libname, "");
+        
+        temp.deleteOnExit();
+        
         OutputStream out = new FileOutputStream(temp);
-
         byte[] buffer = new byte[1024];
-
         int len;
-
         while ((len = in.read(buffer)) != -1) {
             out.write(buffer, 0, len);
         }
@@ -79,11 +98,11 @@ public class SDHash_JNI {
 
     public static String compare(String sdbf) {
         byte[] content = sdbf.getBytes();
-        
+
         ByteBuffer buffer = ByteBuffer.allocateDirect(content.length);
 
         buffer.put(content);
-            
+
         return jni.compare(buffer, 16);
     }
 
